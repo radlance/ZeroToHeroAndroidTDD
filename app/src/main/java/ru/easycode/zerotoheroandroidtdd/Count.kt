d6dbfd2fe21e9e9cd4caf1f6603c34c489d6b95a
Dmitry <manyakindima@gmail.com>
2024-04-29 18:49:32 +0300

    task/011 Task011Test & CountTest solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,13 @@
+package ru.easycode.zerotoheroandroidtdd
+
+interface Count {
+    fun increment(number: String): String
+    class Base(private val step: Int) : Count {
+        init {
+            if(step <= 0) {
+                throw IllegalStateException("step should be positive, but was $step")
+            }
+        }
+        override fun increment(number: String): String = (number.toInt() + step).toString()
+    }
+}
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val settings = applicationContext.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
+        binding.incrementButton.setOnClickListener {
+            val count = Count.Base(step = 2)
+            binding.countTextView.text = count.increment(binding.countTextView.text.toString())
+            settings.edit().putString(NUMBER_KEY, binding.countTextView.text.toString()).apply()
+        }
+
+        if (settings.contains(NUMBER_KEY)) {
+            binding.countTextView.text = settings.getString(NUMBER_KEY, "")
+        }
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        if (savedInstanceState?.containsKey(NUMBER_KEY) == true) {
+            binding.countTextView.text = savedInstanceState.getString(NUMBER_KEY)
+        }
+    }
+
+    companion object {
+        private const val STORAGE_KEY = "storage"
+        private const val NUMBER_KEY = "number"
     }
 }
