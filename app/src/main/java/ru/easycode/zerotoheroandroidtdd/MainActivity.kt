c0bc6217c4671c5cba71ff2a0f5b8e4e9b74c3c9
Dmitry <manyakindima@gmail.com>
2024-04-29 14:21:06 +0300

    task/010 Task010Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,36 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        var mustRemoved = false
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val settings = applicationContext.getSharedPreferences(STORAGE_NAME, Context.MODE_PRIVATE)
+        binding.removeButton.setOnClickListener {
+            settings.edit().putBoolean(EXIST_STATUS, true).apply()
+            binding.rootLayout.removeView(binding.titleTextView)
+            binding.removeButton.isEnabled = false
+        }
+
+        if(settings.contains(EXIST_STATUS)) {
+            mustRemoved = true
+        }
+
+        if(mustRemoved) {
+            binding.rootLayout.removeView(binding.titleTextView)
+            binding.removeButton.isEnabled = false
+        }
+    }
+
+    companion object {
+        private const val STORAGE_NAME = "storage"
+        private const val EXIST_STATUS = "exist_status"
     }
 }
