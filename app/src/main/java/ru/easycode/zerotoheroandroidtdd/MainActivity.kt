5be65138991acfcc3cd670cab05479e7c75f8c16
Dmitry <manyakindima@gmail.com>
2024-05-05 19:54:51 +0300

    task/021 Task021Test solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,36 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.inputEditText.addTextChangedListener {
+            binding.actionButton.isEnabled = binding.inputEditText.text?.length!! >= 3
+        }
+
+        binding.actionButton.setOnClickListener {
+            binding.titleTextView.text = binding.inputEditText.text
+            binding.inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_STRING, binding.titleTextView.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.titleTextView.text = savedInstanceState.getString(KEY_STRING)
+    }
+    companion object {
+        private const val KEY_STRING = "string"
     }
 }
